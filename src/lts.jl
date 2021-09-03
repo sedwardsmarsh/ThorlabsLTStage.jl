405df80f72a47bd0382ca732be804b2b5a6947ed
Steve Leung <steve@orchardultrasound.com>
2021-09-03 14:56:36 -0700

    Combined similar lts and stage files into fewer files

--- src/lts.jl
+++ src/lts.jl
@@ -1,6 +1,3 @@
-include("_lts.jl")
-include("lts3d.jl")
-
 import InstrumentConfig: initialize,terminate
 
 terminate(lts::T) where T <: LTS = close!(lts)
@@ -64,4 +61,111 @@ function setup(stage, stage_config)
         stage_config, "max_acceleration", acceleration(stage)
     )
     acceleration!(stage, max_acc)
+end
+
+
+get_limits(lts) = limits(lts)
+set_limits(lts, lower, upper) = limits!(lts, raw_meters.(lower), raw_meters.(upper))
+
+get_limits_x(lts) = get_limits(lts.x)
+get_limits_y(lts) = get_limits(lts.y)
+get_limits_z(lts) = get_limits(lts.z)
+
+set_limits_x(lts, l, u) = set_limits(lts.x, l, u)
+set_limits_y(lts, l, u) = set_limits(lts.y, l, u)
+set_limits_z(lts, l, u) = set_limits(lts.z, l, u)
+
+move_xyz(lts, x, y, z) = move(lts, raw_meters(x), raw_meters(y), raw_meters(z))
+
+move_x_abs(lts, x) = move_abs(lts.x, x)
+move_y_abs(lts, y) = move_abs(lts.y, y)
+move_z_abs(lts, z) = move_abs(lts.z, z)
+
+move_x_rel(lts, x) = move_rel(lts.x, x)
+move_y_rel(lts, y) = move_rel(lts.y, y)
+move_z_rel(lts, z) = move_rel(lts.z, z)
+
+pos_xyz(lts) = pos(lts)
+pos_x(lts) = pos(lts.x)
+pos_y(lts) = pos(lts.y)
+pos_z(lts) = pos(lts.z)
+
+home(lts) = home_xyz(lts)
+home_x(lts) = home(lts.x)
+home_y(lts) = home(lts.y)
+home_z(lts) = home(lts.z)
+
+get_max_velocity(lts) = map(get_max_velocity, stages(lts))
+get_max_acceleration(lts) = map(get_max_acceleration, stages(lts))
+
+
+
+stages(lts::LTS_3D) = (lts.x, lts.y, lts.z)
+
+function close!(lts::T) where T <: LTS
+    s = stages(lts)
+    map(close!, s)
+
+    return nothing
+end
+
+function limits(lts::T) where T <: LTS
+    s = stages(lts)
+    return map(x->lower_limit(x)*m, s), map(x->upper_limit(x)*m, s)
+end
+
+
+function limits!(lts::T, lower, upper) where T <: LTS
+    s = stages(lts)
+    num_stages = length(s)
+    length(lower) != num_stages && error("Expected $(num_stages) elements in $lower")
+    length(upper) != num_stages && error("Expected $(num_stages) elements in $upper")
+    for i in 1:num_stages
+        set_limits(s[i], lower[i], upper[i])
+    end
+
+    return nothing
+end
+
+function move(lts::LTS_3D, x, y, z; move_func=move_abs!)
+    move_func(lts.x, x; block=false)
+    move_func(lts.y, y; block=false)
+    move_func(lts.z, z; block=false)
+    pause(lts.x, x)
+    pause(lts.y, y)
+    pause(lts.z, z)
+
+    return nothing
+end
+
+home_xyz(lts::LTS_3D) = move(lts, 0, 0, 0)
+
+pos(lts::T) where T <:  LTS = [map(pos, stages(lts))...]
+
+reset_limits(lts::T) where T <: LTS = map(reset_limits, stages(lts))
+
+function Base.show(io::IO, ::MIME"text/plain", lts::T) where T <: LTS
+   println(io, "Thorlabs LTS")
+   function p_stage(io, s)
+       print(io, " ")
+       Base.show(io, s)
+       println() 
+   end
+   p_stage(io, lts.x)
+   p_stage(io, lts.y)
+   p_stage(io, lts.z)
+end
+
+function set_origin(lts::LTS_3D)
+    map(set_origin, stages(lts))
+    return nothing
+end
+
+function get_origin(lts::LTS_3D)
+    return [map(get_origin, stages(lts))...]
+end
+
+function move_to_origin(lts::LTS_3D)
+    map(move_to_origin, stages(lts))
+    return nothing
 end
