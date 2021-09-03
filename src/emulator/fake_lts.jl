c23255bd6203c381cf9fdc456759ef467308cb5a
Steve Leung <steve@orchardultrasound.com>
2021-09-02 18:51:06 -0700

    Reorganized fake_lts.jl to group similar functions with each other

--- src/emulator/fake_lts.jl
+++ src/emulator/fake_lts.jl
@@ -1,33 +1,52 @@
-import InstrumentConfig: initialize,terminate
+import InstrumentConfig: initialize, terminate
 
 mutable struct FakeStage
     serial::String
     info::String
-    min_pos
-    max_pos
-    lower_limit
-    upper_limit
-    max_velocity
-    max_acceleration
-    pos
+    origin_pos::Float64
+    min_pos::Float64
+    max_pos::Float64
+    lower_limit::Float64
+    upper_limit::Float64
     is_moving::Bool
+    # additional fields that Stage does not have
+    current_pos::Float64
+    max_velocity::Float64
+    max_acceleration::Float64
 end
 
 function FakeStage(serial)
-    return FakeStage(serial, "HS FakeLTS 150mm", 0, 0.15, 0, 0.15, 20, 20, 0, false)
+    return FakeStage(serial, "HS FakeLTS 150mm", 0, 0, 0.15, 0, 0.15, false, 0, 20, 20)
 end
 
+struct FakeLTS <: LTS
+    x::FakeStage
+    y::FakeStage
+    z::FakeStage
+end
+
+function initialize(::Type{FakeLTS}) 
+    return FakeLTS(map(FakeStage, ["1", "2", "3"])...)
+end
+
+
+## FakeLTS functions
+stages(lts::FakeLTS) = (lts.x, lts.y, lts.z)
+
+function move(lts::FakeLTS, x, y, z; move_func=false)
+    move_abs!(lts.x, x)
+    move_abs!(lts.y, y)
+    move_abs!(lts.z, z)
+end
+
+home_xyz(lts::FakeLTS) = move(lts, raw_meters(0m), raw_meters(0m), raw_meters(0m))
+
+
+## FakeStage functions
+# position
 home(s::FakeStage) = move_abs!(s, 0)
 
-pos(s::FakeStage) = s.pos * m
-
-get_max_velocity(st::FakeStage) = st.max_velocity
-
-set_max_velocity(st::FakeStage, max::Unitful.Velocity) = st.max_velocity = uconvert(mm/s, max)
-
-get_max_acceleration(st::FakeStage) = st.max_acceleration
-
-set_max_acceleration(st::FakeStage, max::Unitful.Acceleration) = st.max_acceleration = uconvert(mm/s^2, max)
+pos(s::FakeStage) = s.current_pos * m
 
 function move_abs(stage::FakeStage, position::Unitful.Length)
     move_abs!(stage, raw_meters(position))
@@ -36,11 +55,13 @@ end
 
 function move_abs!(s::FakeStage, position)
     check_limits(s, position)
-    s.pos = position
+    s.current_pos = position
 end
 
 move_rel(s::FakeStage, position::Unitful.Length) = move_abs!(s, raw_meters(pos(s)+position))
 
+
+# position limits
 lower_limit(s::FakeStage) = s.lower_limit
 upper_limit(s::FakeStage) = s.upper_limit
 
@@ -56,20 +77,12 @@ function reset_limits(stage::FakeStage)
     stage.lower_limit = stage.min_pos
 end
 
-struct FakeLTS <: LTS
-    x::FakeStage
-    y::FakeStage
-    z::FakeStage
-end
 
-initialize(::Type{FakeLTS}) = FakeLTS(map(FakeStage, ["1", "2", "3"])...)
+# velocity
+get_max_velocity(st::FakeStage) = st.max_velocity
+set_max_velocity(st::FakeStage, max::Unitful.Velocity) = st.max_velocity = uconvert(mm/s, max)
 
-stages(lts::FakeLTS) = (lts.x, lts.y, lts.z)
 
-function move(lts::FakeLTS, x,y,z; move_func=false)
-    move_abs!(lts.x, x)
-    move_abs!(lts.y, y)
-    move_abs!(lts.z, z)
-end
-
-home_xyz(lts::FakeLTS) = move(lts, raw_meters(0m), raw_meters(0m), raw_meters(0m))
+# acceleration
+get_max_acceleration(st::FakeStage) = st.max_acceleration
+set_max_acceleration(st::FakeStage, max::Unitful.Acceleration) = st.max_acceleration = uconvert(mm/s^2, max)
