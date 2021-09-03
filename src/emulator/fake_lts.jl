42e288ab2bff9a151b781c01ecc092e08bdbb073
Steve Leung <steve@orchardultrasound.com>
2021-09-02 18:58:31 -0700

    Expanded fake_lts.jl input variable names to be more informative

--- src/emulator/fake_lts.jl
+++ src/emulator/fake_lts.jl
@@ -44,32 +44,36 @@ home_xyz(lts::FakeLTS) = move(lts, raw_meters(0m), raw_meters(0m), raw_meters(0m
 
 ## FakeStage functions
 # position
-home(s::FakeStage) = move_abs!(s, 0)
+home(stage::FakeStage) = move_abs!(stage, 0)
 
-pos(s::FakeStage) = s.current_pos * m
+pos(stage::FakeStage) = stage.current_pos * m
 
 function move_abs(stage::FakeStage, position::Unitful.Length)
     move_abs!(stage, raw_meters(position))
     return nothing
 end
 
-function move_abs!(s::FakeStage, position)
-    check_limits(s, position)
-    s.current_pos = position
+function move_abs!(stage::FakeStage, position)
+    check_limits(stage, position)
+    stage.current_pos = position
+    return nothing
 end
 
-move_rel(s::FakeStage, position::Unitful.Length) = move_abs!(s, raw_meters(pos(s)+position))
+function move_rel(stage::FakeStage, position::Unitful.Length) 
+    move_abs!(stage, raw_meters(pos(stage)+position))
+    return nothing
+end
 
 
 # position limits
-lower_limit(s::FakeStage) = s.lower_limit
-upper_limit(s::FakeStage) = s.upper_limit
+lower_limit(stage::FakeStage) = stage.lower_limit
+upper_limit(stage::FakeStage) = stage.upper_limit
 
-get_limits(s::FakeStage) = s.lower_limit * m, s.upper_limit * m
+get_limits(stage::FakeStage) = stage.lower_limit * m, stage.upper_limit * m
 
-function set_limits(s::FakeStage, lower, upper)
-    s.lower_limit = ustrip(lower)
-    s.upper_limit = ustrip(upper)
+function set_limits(stage::FakeStage, lower, upper)
+    stage.lower_limit = ustrip(lower)
+    stage.upper_limit = ustrip(upper)
 end
 
 function reset_limits(stage::FakeStage)
@@ -79,10 +83,10 @@ end
 
 
 # velocity
-get_max_velocity(st::FakeStage) = st.max_velocity
-set_max_velocity(st::FakeStage, max::Unitful.Velocity) = st.max_velocity = uconvert(mm/s, max)
+get_max_velocity(stage::FakeStage) = stage.max_velocity
+set_max_velocity(stage::FakeStage, max::Unitful.Velocity) = stage.max_velocity = uconvert(mm/s, max)
 
 
 # acceleration
-get_max_acceleration(st::FakeStage) = st.max_acceleration
-set_max_acceleration(st::FakeStage, max::Unitful.Acceleration) = st.max_acceleration = uconvert(mm/s^2, max)
+get_max_acceleration(stage::FakeStage) = stage.max_acceleration
+set_max_acceleration(stage::FakeStage, max::Unitful.Acceleration) = stage.max_acceleration = uconvert(mm/s^2, max)
