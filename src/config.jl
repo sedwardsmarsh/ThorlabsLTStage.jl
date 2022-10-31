f61bc99ab25bd2fa52ba3a16db42ba6015ffaef4
Yoni Eisips <yoni@snnelectronics.com>
2022-10-31 16:24:46 -0700

    Add preferences.jl to Thorlabs package, implement helper methods

--- src/config.jl
+++ src/config.jl
@@ -1,6 +1,27 @@
 using InstrumentConfig
+using Preferences
 
 const positioner_system_config = InstrumentConfig.Config(".positioner_system_config.yml", @__MODULE__)
+
+function set_preferences(prefs::Pair{String,Any}...)
+    @set_preferences! prefs
+    return nothing
+end
+
+function get_preference(key::String)
+    @load_preference key
+    return nothing
+end
+
+function has_preference(key::String)
+    @has_preference key
+    return nothing
+end
+
+function del_preferences(prefs::Pair{String,Any}...)
+    @delete_preferences! prefs
+    return nothing
+end
 
 function load_config()
     InstrumentConfig.load_config(positioner_system_config)
@@ -20,7 +41,7 @@ function edit_config()
     InstrumentConfig.edit_config(positioner_system_config)
 end
 
-function backend(;v=true) 
+function backend(;v=true)
     b = get(get_config(), "backend", "None")
     if b == "None" && v
         @info """
@@ -43,7 +64,7 @@ function create_aliases(config; ignore=[])
             error("""
             $(config.loaded_file) contains device of name:
                 $device
-            
+
             This is not a valid device.
 
             For a list of available devices use `help> Instrument`
@@ -53,7 +74,7 @@ function create_aliases(config; ignore=[])
         alias = get(data, "alias", "")
         isempty(alias) && continue
         @eval global const $(Symbol(alias)) = $(device_type)
-        @eval export $(Symbol(alias)) 
+        @eval export $(Symbol(alias))
         alias_print("$alias = $device")
     end
 end
@@ -62,4 +83,3 @@ function alias_print(msg)
     printstyled("[ Aliasing: ", color = :blue, bold = true)
     println(msg)
 end
-
