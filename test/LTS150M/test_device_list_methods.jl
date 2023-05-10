ac1b783837eb26314a175ebeecef17066d7446ca
Sam Edwards-Marsh <sam@snnelectronics.com>
2023-05-10 13:30:34 -0700

    added tests for BuildDeviceList() GetDeviceListSize() GetDeviceList()

---
+++ test/LTS150M/test_device_list_methods.jl
@@ -0,0 +1,12 @@
+using Test
+using Unitful
+using ThorlabsLTStage
+
+# instantiate the python backend
+ThorlabsLTStage.init_python_lib()
+
+@testset "test device list methods" begin
+    @test ThorlabsLTStage.BuildDeviceList() == []
+    @test ThorlabsLTStage.GetDeviceListSize() == 3
+    @test length(ThorlabsLTStage.GetDeviceList()) == 3
+end
