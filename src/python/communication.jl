5a687fed4d0cbaae813f78df31587caceeed3f13
Sam Edwards-Marsh <sam@snnelectronics.com>
2023-05-15 12:05:26 -0700

    implemented SetMoveAbsolutePosition(), MoveAbsolute(), MoveAbs(), MoveTo(), GetMotorTravelLimits(), get_stage_axis_min_pos(), get_stage_axis_max_pos(), set_stage_axis_min_max_pos(), GetVelParams(), SetVelParams(), GetVelocity(), SetVelocity(), GetAcceleration(), SetAcceleration()

--- src/python/communication.jl
+++ src/python/communication.jl
@@ -1,3 +1,5 @@
+using Unitful
+
 # initialize the python library
 lts_lib = nothing
 function init_python_lib()
@@ -52,3 +54,90 @@ disconnect_device(serial::String) = 0
 # In the C backend, this function enables the channel corresponding to the 
 # serial number. However, this is handeled by the python backend for us.
 Enable(serial::String) = 0
+
+
+# returns the stage's current position in device units. 
+function GetPos(serial::String)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.get_stage_position_in_device_units(stage)
+end
+
+# set the move absolute position. this function does nothing because the python 
+# backend handles this for us
+SetMoveAbsolutePosition(serial::String, pos::Int64) = 0
+
+# Moves the stage to the position defined in the SetMoveAbsolute command. 
+MoveAbsolute(serial::String) = 0
+
+# move the stage to the specified absolute position.
+function MoveAbs(serial::String, pos::Int64)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.move_stage_absolute(stage, pos)
+end
+
+# move the stage to the specified position, pos (in device units). There is no
+# difference between this method and and absolute movement.
+MoveTo(serial::String, pos::Int64) = MoveAbs(serial, pos)
+
+# Gets the absolute minimum and maximum travel range constants for the current 
+# stage. This functionality is not available in python currently.
+GetMotorTravelLimits(serial::String) = (0u"mm", 150u"mm")
+
+# Gets the minimum position for the stage, in device units
+get_stage_axis_min_pos(serial::String) = 0
+
+# Gets the maxiumum position for the stage, in device units. This functionality 
+# is not supported by the python backend, so we return 150mm in device units.
+# The value returned # is derived from 1mm == 409,600 device units (encoder 
+# counts). 
+get_stage_axis_max_pos(serial::String) = 150 * 409600
+
+# Sets the minimum and maximum position limits for the stage. This functionality # is not available through the python backend, so 0 is returned.
+set_stage_axis_min_max_pos(serial::String, min_pos::Int, max_pos::Int) = 0
+
+# Gets the move velocity parameters.
+function GetVelParams(serial::String)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    acc = lts_lib.get_stage_velocity(stage)
+    vel = lts_lib.get_stage_acceleration(stage)
+    return acc[], vel[]
+end
+
+# Sets the move velocity parameters, in device units
+function SetVelParams(serial::String, acc::Int, vel::Int)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    lts_lib.set_stage_acceleration(stage, acc)
+    lts_lib.set_stage_velocity(stage, vel)
+end
+
+# Get the move velocity.
+function GetVelocity(serial::String)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.get_stage_velocity(stage)
+end
+
+# Set the move velocity.
+function SetVelocity(serial::String, vel::Int)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.set_stage_velocity(stage, vel)
+end
+
+# Get the move acceleration.
+function GetAcceleration(serial::String)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.get_stage_acceleration(stage)
+end
+
+# Set the move acceleration.
+function SetAcceleration(serial::String, acc::Int)
+    global lts_lib
+    stage = lts_lib.STAGE_SERIAL_MAP[serial]
+    return lts_lib.set_stage_acceleration(stage, acc)
+end
