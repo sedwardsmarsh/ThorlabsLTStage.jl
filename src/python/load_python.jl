3d4bacae6636229e1d148da36914e42a71d77163
samsnn <sam@snnelectronics.com>
2023-05-25 11:03:05 -0700

    added find_python3_path() for windows and linux support

--- src/python/load_python.jl
+++ src/python/load_python.jl
@@ -1,7 +1,15 @@
 using Pkg
 
+function find_python3_path()
+    if Base.Sys.iswindows()
+        ENV["PYTHON"] = readchomp(`where python`)
+    elseif Base.Sys.islinux()
+        ENV["PYTHON"] = readchomp(`which python3`)
+    end
+end
+
 function load_python_venv()
-    ENV["PYTHON"] = "/usr/bin/python3"
+    find_python3_path()
     Pkg.build("PyCall")
     @info "Python Loaded Please Restart Julia"
     exit()
