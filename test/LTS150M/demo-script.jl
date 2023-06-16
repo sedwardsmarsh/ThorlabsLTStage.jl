81d071e7be865f586c19385bcfda38d73e020b92
Sam Edwards-Marsh <sam@snnelectronics.com>
2023-06-15 17:33:09 -0700

    Refactor demo script

--- test/LTS150M/demo-script.jl
+++ test/LTS150M/demo-script.jl
@@ -2,30 +2,33 @@ using Test
 using Unitful
 using ThorlabsLTStage
 
+@info "Initializing PositionerSystem"
 ps = ThorlabsLTStage.initialize(PositionerSystem)
 
 @info "Homing PS ..."
+
+ThorlabsLTStage.SetVelParams(ps.x.serial, 40, 5)
+ThorlabsLTStage.SetVelParams(ps.y.serial, 5, 3)
+ThorlabsLTStage.SetVelParams(ps.z.serial, 40, 5)
+
 ThorlabsLTStage.home(ps)
 
-ThorlabsLTStage.SetVelParams(ps.x.serial, 10, 10)
-ThorlabsLTStage.SetVelParams(ps.y.serial, 10, 10)
-ThorlabsLTStage.SetVelParams(ps.z.serial, 10, 10)
-sleep(2)
+ThorlabsLTStage.SetVelParams(ps.x.serial, 50, 50)
+ThorlabsLTStage.SetVelParams(ps.z.serial, 50, 50)
+
 target_position_start = 1.05*u"mm"
-ThorlabsLTStage.move_x_abs(ps, target_position_start)
-ThorlabsLTStage.move_y_abs(ps, target_position_start)
-ThorlabsLTStage.move_z_abs(ps, target_position_start)
-sleep(2)
+ThorlabsLTStage.move_xyz(ps, target_position_start, target_position_start, target_position_start)
+@info "Moved to $target_position_start"
 target_position_middle = 55 *u"mm"
-ThorlabsLTStage.move_x_abs(ps, target_position_middle)
-ThorlabsLTStage.move_y_abs(ps, target_position_middle)
-ThorlabsLTStage.move_z_abs(ps, target_position_middle)
-sleep(2)
+ThorlabsLTStage.move_xyz(ps, target_position_middle, target_position_middle, target_position_middle)
+@info "Moved to $target_position_middle"
 target_position_end = 148*u"mm"
-ThorlabsLTStage.move_x_abs(ps, target_position_end)
-ThorlabsLTStage.move_y_abs(ps, target_position_end)
-ThorlabsLTStage.move_z_abs(ps, target_position_end)
-sleep(2)
+ThorlabsLTStage.move_xyz(ps, target_position_end, target_position_end, target_position_end)
+@info "Moved to $target_position_end"
+
+ThorlabsLTStage.SetVelParams(ps.x.serial, 40, 5)
+ThorlabsLTStage.SetVelParams(ps.z.serial, 40, 5)
+
 ThorlabsLTStage.home(ps)
 
 terminate(ps)
