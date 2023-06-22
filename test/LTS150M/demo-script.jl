e43c36f7a32a2d54e2168eb58d2546cd47420d17
Sam Edwards-Marsh <sam@snnelectronics.com>
2023-06-21 19:57:34 -0700

    Refactored demo script

--- test/LTS150M/demo-script.jl
+++ test/LTS150M/demo-script.jl
@@ -2,34 +2,42 @@ using Test
 using Unitful
 using ThorlabsLTStage
 
-@info "Initializing PositionerSystem"
+@info "Initializing Positioner System"
 ps = ThorlabsLTStage.initialize(PositionerSystem)
 
-@info "Homing PS ..."
+# Set stages velocities
+velocity = 20
+acceleration = 20
+@info "Set move velocity: $velocity mm/sec and move acceleration: $acceleration mm/sec^2"
+ThorlabsLTStage.SetVelParams(ps.x.serial, velocity, acceleration)
+ThorlabsLTStage.SetVelParams(ps.y.serial, velocity, acceleration)
+ThorlabsLTStage.SetVelParams(ps.z.serial, velocity, acceleration)
+# Set home velocity
+home_velocity = 5
+@info "Set home velocity: $home_velocity  mm/sec"
+ThorlabsLTStage.set_home_velocity(ps.x, home_velocity)
+ThorlabsLTStage.set_home_velocity(ps.y, home_velocity)
+ThorlabsLTStage.set_home_velocity(ps.z, home_velocity)
 
-ThorlabsLTStage.SetVelParams(ps.x.serial, 40, 5)
-ThorlabsLTStage.SetVelParams(ps.y.serial, 5, 3)
-ThorlabsLTStage.SetVelParams(ps.z.serial, 40, 5)
-
-ThorlabsLTStage.home(ps)
-
-ThorlabsLTStage.SetVelParams(ps.x.serial, 50, 50)
-ThorlabsLTStage.SetVelParams(ps.z.serial, 50, 50)
+@info "True home Positioner System ..."
+ThorlabsLTStage.true_home(ps.x)
+ThorlabsLTStage.true_home(ps.y)
+ThorlabsLTStage.true_home(ps.z)
 
 target_position_start = 1.05*u"mm"
+@info "Moving to $target_position_start"
 ThorlabsLTStage.move_xyz(ps, target_position_start, target_position_start, target_position_start)
-@info "Moved to $target_position_start"
+
 target_position_middle = 55 *u"mm"
+@info "Moving to $target_position_middle"
 ThorlabsLTStage.move_xyz(ps, target_position_middle, target_position_middle, target_position_middle)
-@info "Moved to $target_position_middle"
+
 target_position_end = 148*u"mm"
+@info "Moving to $target_position_end"
 ThorlabsLTStage.move_xyz(ps, target_position_end, target_position_end, target_position_end)
-@info "Moved to $target_position_end"
 
-ThorlabsLTStage.SetVelParams(ps.x.serial, 40, 5)
-ThorlabsLTStage.SetVelParams(ps.z.serial, 40, 5)
-
+@info "Homing Positioner System ..."
 ThorlabsLTStage.home(ps)
 
 terminate(ps)
-@info "Terminated PS"
+@info "Terminated Positioner System"
